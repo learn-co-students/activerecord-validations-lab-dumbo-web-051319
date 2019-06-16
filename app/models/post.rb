cd2a6b71ca5b0e4047d9ab97ae3915fb14f4d36f
github username <github email address>
2019-06-16 02:42:31 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid size" }
+  # validates :category, acceptance: { accepts: "Fiction" || "Non-Fiction"}
+
+  validate :must_be_clickbaity
+
+  def must_be_clickbaity
+    if !/Won't Believe|Secret|Top\s*\d+|Guess/i.match(title)
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
