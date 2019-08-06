8f3cb87c3adcbaa9617116acda8a1e727d4022e9
Kenton Archer <kenton8654@yahoo.com>
2019-08-05 20:21:52 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,11 @@
 class Author < ActiveRecord::Base
+# No two authors have the same name
+# Author phone numbers are exactly ten digits
+# All posts have a title
+# Post content is at least 250 characters long
+# Post summary is a maximum of 250 characters
+validates :name, presence: true
+validates :name, uniqueness: true
+validates :phone_number, length: { is: 10 }
+
 end
